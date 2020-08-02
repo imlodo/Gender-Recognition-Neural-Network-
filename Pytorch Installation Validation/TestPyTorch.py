7bfd842adbc97f729297b4a555f1873d369338ef
lodo744 <55733870+lodo744@users.noreply.github.com>
2020-08-02 19:06:33 +0200

    Update

--- Pytorch Installation Validation/TestPyTorch.py
+++ Pytorch Installation Validation/TestPyTorch.py
@@ -7,5 +7,4 @@ print(torch.cuda.is_available())
 #Verifichiamo che venga creato un tensore 2x2
 print(torch.rand(2,2))
 
-#Nel caso in cui la prima print restituisce False, allora bisogna eseguire il debug dell'installazione di CUDA, in modo
-#che PyTorch possa rilevare la scheda grafica.
+#Nel caso in cui la prima print restituisce False, allora bisogna eseguire il debug dell'installazione di CUDA, in modo che PyTorch possa rilevare la scheda grafica.
